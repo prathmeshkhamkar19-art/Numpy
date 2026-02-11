21eba24754c061efb3639ea3c3b142a6b375cc04
Prathmesh Khamkar <prathmeshkhamkar19>
2026-02-11 22:00:23 +0530

    CODE is done forto check pro of array in numpy

--- numpy_prop.py
+++ numpy_prop.py
@@ -1,56 +1,56 @@
 # # checking the nnumpy propertie
 import numpy as np
 
-# class Pro:
-#     def __init__(self):     #Constuctor
-#      self.arr_1d =np.array([1,2,33,77])      # 1 Dimension array 
-#      self.arr_2d =np.array([[1,3,5],[34,5,6]])  # 2 Dimension array of 2(row) * 3(col)
+class Pro:
+    def __init__(self):     #Constuctor
+     self.arr_1d =np.array([1,2,33,77])      # 1 Dimension array 
+     self.arr_2d =np.array([[1,3,5],[34,5,6]])  # 2 Dimension array of 2(row) * 3(col)
 
-#      print("Select choise to print Prop of Array:\n")
-#      print("1.for 1 D. ")
-#      print("2.for 2D. ")
-#      print("3.for 3D.")
-#      self.arr_op = int(input("Enter :"))
+     print("Select choise to print Prop of Array:\n")
+     print("1.for 1 D. ")
+     print("2.for 2D. ")
+     print("3.for 3D.")
+     self.arr_op = int(input("Enter :"))        
 
      
-#      print("1.Check Shape")
-#      print("1.Check size")
-#      print("1.Check ndim")
-#      print("1.Check dtype")
-#      self. opt =int(input("Enter the Choise to Check prop :\n"))
+     print("1.Check Shape") 
+     print("1.Check size")
+     print("1.Check ndim")
+     print("1.Check dtype")
+     self. opt =int(input("Enter the Choise to Check prop :\n"))  # input 
      
      
-     
-#      def aray_s(self):
-#        if self.arr_op == 1 :
-#           return self.chk_1d(),None
-#        elif self.arr_op == 2 :
-#           return None,self.chk_2d()
-#        else:
-#           return self.chk_1d(),self.chk_2d()
+      
+     def aray_s(self):        
+       if self.arr_op == 1 :
+          return self.chk_1d(),None
+       elif self.arr_op == 2 :
+          return None,self.chk_2d()
+       else:
+          return self.chk_1d(),self.chk_2d()
           
-#     def chk_1d(self):
-#        if self.opt == 1:
-#           print("Shape of 1d - ",self.arr_1d.shape)
-#        elif self.opt == 2:
-#           print("size of 1d - ",self.arr_1d.size)
-#        elif self.opt == 3:
-#           print("Dimension of 1d - ",self.arr_1d.ndim)
-#        elif self.opt == 4:
-#           print("Datatype of 1 d array is : ",self.arr_1d.dtype)
+    def chk_1d(self):
+       if self.opt == 1:
+          print("Shape of 1d - ",self.arr_1d.shape)
+       elif self.opt == 2:
+          print("size of 1d - ",self.arr_1d.size)
+       elif self.opt == 3:
+          print("Dimension of 1d - ",self.arr_1d.ndim)
+       elif self.opt == 4:
+          print("Datatype of 1 d array is : ",self.arr_1d.dtype)
           
-#     def chk_2d(self):
-#         if self.opt == 1:
-#           print("Shape of 2d - ",self.arr_2d.shape)
-#         elif self.opt == 2:
-#            print("size of 2d - ",self.arr_2d.size)
-#         elif self.opt == 3:
-#           print("Dimension of 2d - ",self.arr_2d.ndim)
-#         elif self.opt == 4:
-#           print("Datatype of 2d array is : ",self.arr_2d.dtype)
+    def chk_2d(self):
+        if self.opt == 1:
+          print("Shape of 2d - ",self.arr_2d.shape)
+        elif self.opt == 2:
+           print("size of 2d - ",self.arr_2d.size)
+        elif self.opt == 3:
+          print("Dimension of 2d - ",self.arr_2d.ndim)
+        elif self.opt == 4:
+          print("Datatype of 2d array is : ",self.arr_2d.dtype)
 
-# p1 = Pro()
-# p1.aray_s()
+p1 = Pro()
+p1.aray_s()
           
           
     
